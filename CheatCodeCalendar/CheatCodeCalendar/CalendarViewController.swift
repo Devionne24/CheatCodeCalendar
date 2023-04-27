b1664d3338a9b6750c72fdac319fa65fff41b2bb
Devionne24 <93213848+Devionne24@users.noreply.github.com>
2023-04-27 12:34:49 -0400

    Add files via upload

---
+++ CheatCodeCalendar/CheatCodeCalendar/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarViewController.swift
+//  CheatCodeCalendar
+//
+//  Created by Guest User on 4/27/23.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    
+    @IBAction func prevMonth(_ sender: Any) {
+    }
+    
+    @IBAction func nextMonth(_ sender: Any) {
+    }
+    
+}
