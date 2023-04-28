5e8e6c1c99216518743b6395f450f08513ac5365
Devionne24 <93213848+Devionne24@users.noreply.github.com>
2023-04-28 12:55:37 -0400

    Add files via upload

---
+++ CheatCodeCalendar/CheatCodeCalendar/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  CheatCodeCalendar
+//
+//  Created by Guest User on 4/27/23.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
