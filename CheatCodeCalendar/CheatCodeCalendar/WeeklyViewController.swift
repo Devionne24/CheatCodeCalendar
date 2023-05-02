72a5c69fb730e1aefbcba61dc789c33db07556d0
Devionne24 <93213848+Devionne24@users.noreply.github.com>
2023-05-02 12:42:33 -0400

    Add files via upload

---
+++ CheatCodeCalendar/CheatCodeCalendar/WeeklyViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CalendarViewController.swift
+//  CheatCodeCalendar
+//
+//  Created by Guest User on 4/27/23.
+//
+
+import UIKit
+import EventKit
+
+class WeeklyViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var selectedDate = Date()
+    var totalSquares = [Date]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCellsView()
+        setMonthView()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setCellsView() {
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
+        
+        let date = totalSquares[indexPath.item]
+        cell.dayOfMonth.text = String(CalendarHelper().daysOfMonth(date: date))
+        
+        if(date == selectedDate) {
+            cell.backgroundColor = UIColor.systemGreen
+        } else {
+            cell.backgroundColor = UIColor.white
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedDate = totalSquares[indexPath.item]
+        collectionView.reloadData()
+    }
+    
+    func setMonthView() {
+        totalSquares.removeAll()
+        
+        var current = CalendarHelper().sundayForDate(date: selectedDate)
+        let nextSunday = CalendarHelper().addDays(date: current, days: 7)
+        
+        while(current < nextSunday) {
+            totalSquares.append(current)
+            current = CalendarHelper().addDays(date: current, days: 1)
+        }
+        
+        monthLabel.text = CalendarHelper().monthString(date: selectedDate) + " " + CalendarHelper().yearString(date: selectedDate)
+        
+        collectionView.reloadData()
+    }
+    
+    @IBAction func prevWeek(_ sender: Any) {
+        selectedDate = CalendarHelper().addDays(date: selectedDate, days: -7)
+        setMonthView()
+    }
+    
+    @IBAction func nextWeek(_ sender: Any) {
+        selectedDate = CalendarHelper().addDays(date: selectedDate, days: 7)
+        setMonthView()
+    }
+    
+    
+}
+
