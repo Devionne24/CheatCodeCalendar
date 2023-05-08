6180779d0889b27145e2402ed179f330b7a5d944
Devionne24 <93213848+Devionne24@users.noreply.github.com>
2023-05-08 11:44:56 -0400

    Add files via upload

---
+++ CheatCodeCalendar/CheatCodeCalendar/EventCell.swift
@@ -0,0 +1,12 @@
+//
+//  EventCell.swift
+//  CheatCodeCalendar
+//
+//  Created by Guest User on 5/5/23.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    @IBOutlet weak var eventLabel: UILabel!
+}
